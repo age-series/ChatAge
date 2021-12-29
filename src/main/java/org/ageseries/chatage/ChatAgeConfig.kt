87eac45d3e9b7883295656333b883b4427464ba0
Jared Dunbar <jrddunbr@ja4.org>
2021-12-29 11:39:00 -0500

    Broken but pushing up to work on it

--- src/main/java/org/ageseries/chatage/ChatAgeConfig.kt
+++ src/main/java/org/ageseries/chatage/ChatAgeConfig.kt
@@ -1,34 +1,33 @@
 package org.ageseries.chatage
 
+import com.charleskorn.kaml.Yaml
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
 import java.io.File
 
 object ChatAgeConfig {
-    private val CONFIG_FILE: File = File("config/chat_age.json")
+    private val CONFIG_FILE: File = File("config/chat_age.yaml")
     var config: ChatAgeData = ChatAgeData()
 
     fun loadConfig(configFile: File = CONFIG_FILE) {
         try {
             if (configFile.isFile) {
                 LOGGER.info("[Chat Age] Reading config from ${configFile.absoluteFile}")
-                config = Json.decodeFromString(ChatAgeData.serializer(), configFile.readLines().joinToString("\n"))
-                //config = Yaml.default.decodeFromStream(ChatAgeData.serializer(), configFile.inputStream())
+                config = Yaml.default.decodeFromStream(ChatAgeData.serializer(), configFile.inputStream())
             } else {
                 config = ChatAgeData()
                 saveConfig()
             }
         } catch (e: Exception) {
             LOGGER.error("Chat Age had an issue with loading the configuration file, please check the file for errors.")
-            LOGGER.error("Check that 1) You have valid JSON 2) the config directives are spelled correctly (see documentation)")
+            LOGGER.error("Check that 1) You have valid YAML 2) the config directives are spelled correctly (see documentation)")
+            LOGGER.error(e)
         }
     }
 
     private fun saveConfig(configFile: File = CONFIG_FILE) {
         try {
             LOGGER.info("[Chat Age] Writing config to ${configFile.absoluteFile}")
-            val configText = Json.encodeToString(ChatAgeData.serializer(), config)
-            //val configText = Yaml.default.encodeToString(ChatAgeData.serializer(), config)
+            val configText = Yaml.default.encodeToString(ChatAgeData.serializer(), config)
             if (!configFile.exists()) {
                 configFile.createNewFile()
             }
